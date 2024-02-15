d7ed53267ffcaddb53b76875bb7926b440b724f8
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-15 15:54:34 +0100

    rem maincontroller + add comiccontroller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MainController;
+use App\Http\Controllers\ComicController;
 
-Route::get('/', [MainController :: class, 'index']);
+Route::get('/', [ComicController :: class, 'index']);
