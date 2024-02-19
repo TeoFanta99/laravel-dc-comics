bd5c392f23cd53e2164c695e5e69f300273a2f48
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-19 16:54:41 +0100

    add validations

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -15,12 +15,24 @@
     <label for="title">Titolo</label>
     <input type="text" name="title" id="title">
     <br>
+    @error ('title')
+        <div class="alert alert-danger">{{$message}}</div>
+    @enderror
+
     <label for="author">Autore</label>
     <input type="text" name="author" id="author">
     <br>
+    @error ('author')
+        <div class="alert alert-danger">{{$message}}</div>
+    @enderror
+
     <label for="price">Prezzo</label>
     <input type="text" name="price" id="price">
     <br>
+    @error ('price')
+        <div class="alert alert-danger">{{$message}}</div>
+    @enderror
+
     <input type="submit" value="CREATE">
 </form>
 
