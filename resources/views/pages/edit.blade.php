537e7b35f2f8a0d112519a9b642a786c93bf5ce6
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-19 14:58:47 +0100

    add alert

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -7,6 +7,18 @@
     [{{$comic -> id}}]
     EDIT COMIC
 </h1>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+
 <form 
     action="{{route('users.update', $comic -> id)}}"
     method="POST"
